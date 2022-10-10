125e04e6318251cb26b36b6af3109b220130a13e
eai04191 <eai04191@users.noreply.github.com>
2022-10-10 15:13:30 +0000

    update with 002928b0bcc26627452f70c9fc87242e5d57b840

--- eralao-jp.user.js
+++ eralao-jp.user.js
@@ -46,46 +46,59 @@
   styleInject(css_248z);
 
   (function () {
-      document.documentElement.setAttribute("lang", "ko");
+      document.documentElement.setAttribute("lang", "choise");
+      var titleButtonTranslations = [
+          { string: "Start", ja: "はじめから" },
+          { string: "Load", ja: "つづきから" },
+          { string: "Settings", ja: "オプション" },
+          { string: "Credits", ja: "クレジット" },
+          { string: "Gallery", ja: "ギャラリー" },
+      ];
+      titleButtonTranslations.forEach(function (_a) {
+          var string = _a.string, ja = _a.ja;
+          var css = "main-menu button[string=\"".concat(string, "\"] { font-size: 0; }") +
+              "main-menu button[string=\"".concat(string, "\"] > span > font::before { content: \"").concat(ja, "\"; font-size: var(--button-font-size); }");
+          GM_addStyle(css);
+      });
       var buttonTranslations = [
           // 1日のコマンド
-          { choise: "연애", translate: "デート" },
-          { choise: "외출", translate: "外出" },
-          { choise: "상태보기", translate: "ステータス" },
-          { choise: "출격", translate: "出撃" },
-          { choise: "휴식", translate: "休息" },
-          { choise: "대상변경", translate: "バイオロイド変更" },
-          { choise: "요리", translate: "料理" },
-          { choise: "헤어지기", translate: "別れる" },
+          { choise: "연애", ja: "デート" },
+          { choise: "외출", ja: "外出" },
+          { choise: "상태보기", ja: "ステータス" },
+          { choise: "출격", ja: "出撃" },
+          { choise: "휴식", ja: "休息" },
+          { choise: "대상변경", ja: "バイオロイド変更" },
+          { choise: "요리", ja: "料理" },
+          { choise: "헤어지기", ja: "別れる" },
           // デートコマンド
-          { choise: "Talk", translate: "雑談" },
-          { choise: "Joke", translate: "エッチな冗談" },
-          { choise: "Hand", translate: "手をつなぐ" },
-          { choise: "Head", translate: "頭を撫でる" },
-          { choise: "Tickle", translate: "くすぐる" },
-          { choise: "Onmylap", translate: "膝枕" },
-          { choise: "Onyourlap", translate: "膝枕してもらう" },
-          { choise: "Hug", translate: "ハグ" },
-          { choise: "Backhug", translate: "後ろからハグ" },
-          { choise: "Onarm", translate: "腕枕" },
-          { choise: "Peck", translate: "チュー" },
-          { choise: "Tummy", translate: "お腹を触る" },
-          { choise: "Ear", translate: "耳を噛む" },
-          { choise: "Kiss", translate: "キス" },
-          { choise: "Neck", translate: "首を舐める" },
-          { choise: "Back", translate: "背中を舐める" },
-          { choise: "Pahu", translate: "パフパフ" },
-          { choise: "Fword", translate: "悪口を言う" },
-          { choise: "Cheek", translate: "頬を殴る" },
-          { choise: "Bodyblow", translate: "腹を殴る" },
-          { choise: "Kick", translate: "蹴る" },
+          { choise: "Talk", ja: "雑談" },
+          { choise: "Joke", ja: "エッチな冗談" },
+          { choise: "Hand", ja: "手をつなぐ" },
+          { choise: "Head", ja: "頭を撫でる" },
+          { choise: "Tickle", ja: "くすぐる" },
+          { choise: "Onmylap", ja: "膝枕" },
+          { choise: "Onyourlap", ja: "膝枕してもらう" },
+          { choise: "Hug", ja: "ハグ" },
+          { choise: "Backhug", ja: "後ろからハグ" },
+          { choise: "Onarm", ja: "腕枕" },
+          { choise: "Peck", ja: "チュー" },
+          { choise: "Tummy", ja: "お腹を触る" },
+          { choise: "Ear", ja: "耳を噛む" },
+          { choise: "Kiss", ja: "キス" },
+          { choise: "Neck", ja: "首を舐める" },
+          { choise: "Back", ja: "背中を舐める" },
+          { choise: "Pahu", ja: "パフパフ" },
+          { choise: "Fword", ja: "悪口を言う" },
+          { choise: "Cheek", ja: "頬を殴る" },
+          { choise: "Bodyblow", ja: "腹を殴る" },
+          { choise: "Kick", ja: "蹴る" },
           // その他の選択肢
-          { choise: "Sell", translate: "売る" },
+          { choise: "Sell", ja: "売る" },
       ];
       buttonTranslations.forEach(function (_a) {
-          var choise = _a.choise, translate = _a.translate;
+          var choise = _a.choise, ja = _a.ja;
           var css = "button[data-choice=\"".concat(choise, "\"] > font { font-size: 0; }") +
-              "button[data-choice=\"".concat(choise, "\"] > font::before { content: \"").concat(translate, "\"; font-size: var(--button-font-size); }");
+              "button[data-choice=\"".concat(choise, "\"] > font::before { content: \"").concat(ja, "\"; font-size: var(--button-font-size); }");
           GM_addStyle(css);
       });
   })();
